f283de66a55d8b7cb8e4c7fbe4fa88d5d612501c
Oliver Watkins <oliver.f.watkins@gmail.com>
2020-08-03 13:54:30 +0200

    doc

--- src/js/README.md
+++ src/js/README.md
@@ -1,1 +1,6 @@
 Shape shop
+
+
+http://localhost:8080/h2-console/
+
+To log in to the h2 console: "sa", ""
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,1 @@
-create table bananas(
-	username varchar_ignorecase(50) not null primary key,
-	password varchar_ignorecase(200) not null,
-	enabled boolean not null
-);
-
-create table blahblahs (
-	username varchar_ignorecase(50) not null,
-	authority varchar_ignorecase(50) not null,
-);
+/* symbol and ends with */
