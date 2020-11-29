3243a6148085b3a0374748f49394b50db9f29f0d
owatkins.ext <oliver.watkins@external.mgm-tp.com>
2020-11-29 15:05:02 +0100

    H2 DB working again. commented out mySQL

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -2,8 +2,8 @@
 
 /* symbol and ends with */
 
-CREATE TABLE IF NOT EXISTS employee (
-  empId VARCHAR(10) NOT NULL,
-  empName VARCHAR(100) NOT NULL
-);
-
+-- CREATE TABLE IF NOT EXISTS employee (
+--   empId VARCHAR(10) NOT NULL,
+--   empName VARCHAR(100) NOT NULL
+-- );
+--
