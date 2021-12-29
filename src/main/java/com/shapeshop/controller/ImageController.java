5a8862a1fc0e186792741ea8a2afa0aaef2e2f23
oliverwatkins <oliver.f.watkins@gmail.com>
2021-12-29 13:09:06 +1100

    update image working again

--- src/main/java/com/shapeshop/controller/ImageController.java
+++ src/main/java/com/shapeshop/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.shapeshop.controller;
 import com.shapeshop.entity.ProductEntity;
 import com.shapeshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,9 +30,36 @@ public class ImageController {
 
     private static File writeFileToFileSystem(MultipartFile file) throws IOException {
 
-        String fileLocation = new File("src\\main\\resources\\alpenhof\\image").getAbsolutePath() + "\\" + file.getOriginalFilename();
 
-        File convFile = new File(fileLocation);
+        File file2 = new ClassPathResource("countries.xml").getFile();
+
+        if (!file2.exists()) {
+            throw new RuntimeException("!file2.exists()");
+        }
+
+
+        File file3 = new ClassPathResource("static\\images\\alpenhof\\testfile.xml").getFile();
+
+        if (!file3.exists()) {
+            throw new RuntimeException("!file3.exists()");
+        }
+
+
+        File file4 = new ClassPathResource("static\\images\\alpenhof\\").getFile();
+
+        if (!file4.isDirectory()) {
+            throw new RuntimeException("!file4.isDirectory()");
+        }
+
+        File file5 = new File(file4.getPath() + "\\" + "myNewFile.txt");
+        if (file5.createNewFile()) {
+            System.out.println("File created: " + file5.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
+
+        File convFile = new File(file4.getPath() + "\\" + file.getOriginalFilename());
+        convFile.createNewFile();
 
         FileOutputStream fos = new FileOutputStream(convFile);
 
